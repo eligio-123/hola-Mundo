77eef47ead3f6b51fb391ae60ff023e0c8798bac
Eligio <leligio.cortes@alumnos.udg.mx>
2019-10-16 20:19:25 -0500

    alerta funcionando con 2 botones

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,11 +8,18 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
   nombre = '';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    public alertCtrl: AlertController) {
 
   }
   click() {
-    console.log('hola', this.nombre);
+    //console.log('hola', this.nombre);
+    const alerta = this.alertCtrl.create({
+      title: 'Hola',
+      subTitle: this.nombre,
+      buttons: ['OK', 'Cancel']
+    });
+    alerta.present();
   }
 
 }
